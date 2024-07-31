1cfc38f4689470a0b03ba1fffdd0c66c84b83829
Eun-young Choi <youngchoi2094@gmail.com>
2024-07-31 14:23:48 +0200

    Update site.config.js
    Change the year

--- site.config.js
+++ site.config.js
@@ -3,7 +3,7 @@ const CONFIG = {
   profile: {
     name: "Eun-young Choi",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "Aspringfrontend developer",
+    role: "Aspring frontend developer",
     bio: "I develop everything using node.",
     email: "youngchoi2094@gmail.com",
     linkedin: "y0ungchoi",
@@ -25,7 +25,7 @@ const CONFIG = {
 
   // CONFIG configration (required)
   link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
+  since: 2024, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
