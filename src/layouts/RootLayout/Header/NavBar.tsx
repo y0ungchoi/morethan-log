32e41f195c622a5f574a16d4940091e6c2f40d37
Eun Young Choi <eun-young.choi@s2022.tu-chemnitz.de>
2025-03-22 21:51:51 +0100

    Add Choco navbar

--- src/layouts/RootLayout/Header/NavBar.tsx
+++ src/layouts/RootLayout/Header/NavBar.tsx
@@ -1,17 +1,28 @@
 import styled from "@emotion/styled"
 import Link from "next/link"
+import { useState } from "react"
 
 const NavBar: React.FC = () => {
   const links = [{ id: 1, name: "About", to: "/about" }]
+  const [toggleChoco, setToggleChoco] = useState(false)
+
+  const handleToggleChoco = () => {
+    setToggleChoco(!toggleChoco)
+  }
   return (
     <StyledWrapper className="">
-      <ul>
-        {links.map((link) => (
-          <li key={link.id}>
-            <Link href={link.to}>{link.name}</Link>
-          </li>
-        ))}
-      </ul>
+      <button onClick={handleToggleChoco}>Toggle Choco</button>
+      {toggleChoco && (
+        <div className="choco">
+          <ul>
+            {links.map((link) => (
+              <li key={link.id}>
+                <Link href={link.to}>{link.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </StyledWrapper>
   )
 }
@@ -20,6 +31,15 @@ export default NavBar
 
 const StyledWrapper = styled.div`
   flex-shrink: 0;
+  .choco {
+    font-size: 1.5rem;
+    width: 100%;
+    text-align: center;
+    position: absolute;
+    background-color: ${({ theme }) => theme.colors.bronze11};
+    top: 3rem;
+    right: 0rem;
+  }
   ul {
     display: flex;
     flex-direction: row;
